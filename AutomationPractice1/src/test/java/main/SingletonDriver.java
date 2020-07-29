63b8d527bdf0297f1f6325a044aef4dbf271d402
Fariha_Samsi <tasnim.samsi@gmail.com>
2020-07-28 22:47:41 -0400

    Second commit!

--- AutomationPractice1/src/test/java/main/SingletonDriver.java
+++ AutomationPractice1/src/test/java/main/SingletonDriver.java
@@ -9,55 +9,76 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SingletonDriver {
 
-	private WebDriver driver ;	
+	protected static WebDriver driver  = null;	
+	private static SingletonDriver driverInstance = null;
+	
 	
 	public SingletonDriver(WebDriver driver) {
 		
-		this.driver = driver;
+		SingletonDriver.driver = driver;
 		
 	}
+	
+	
+	public static SingletonDriver getThisClassInstance() {
+		
+		if(driverInstance == null) 
+			driverInstance = new SingletonDriver(driver);
+		
+		return driverInstance;
+		
+	}
+	
 	
 	public WebDriver getDriver() {
 		return driver;
 	}
-
+	/*
+	 * public static void open() { openBrowser("sd","f"); }
+	 */
 
 	public WebDriver openBrowser(String browser, String url) {
 		
-		if(browser.equalsIgnoreCase("Chrome")) {
-			
-			driver = new ChromeDriver();
-			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-			driver.manage().window().maximize();
-			driver.get("https://www.google.com/");
-			driver.navigate().to(url);
-		}
-		else if(browser.equalsIgnoreCase("Firefox")) {
-			
-			driver = new FirefoxDriver();
-			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-			driver.manage().window().maximize();
-			driver.get("https://www.google.com/");
-			driver.navigate().to(url);
+		if(driver == null) {
+			if(browser.equalsIgnoreCase("Chrome")) {
+				
+				driver = new ChromeDriver();
+				driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+				driver.manage().window().maximize();
+				driver.get("https://www.google.com/");
+				driver.navigate().to(url);
+			}
+			else if(browser.equalsIgnoreCase("Firefox")) {
+				
+				driver = new FirefoxDriver();
+				driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+				driver.manage().window().maximize();
+				driver.get("https://www.google.com/");
+				driver.navigate().to(url);
 
-		}
-		else if(browser.equalsIgnoreCase("Edge")) {
-			
-			driver = new EdgeDriver();
-			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-			driver.manage().window().maximize();
-			driver.get("https://www.google.com/");
-			driver.navigate().to(url);
+			}
+			else if(browser.equalsIgnoreCase("Edge")) {
+				
+				driver = new EdgeDriver();
+				driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+				driver.manage().window().maximize();
+				driver.get("https://www.google.com/");
+				driver.navigate().to(url);
 
-		}		
-		else
-		{
-			System.out.println("Incorrect driver");
+			}		
+			else
+			{
+				System.out.println("Incorrect driver");
+			}
 		}
+		
+		
+		
 		
 		return driver;
 	}
--- AutomationPractice1/src/test/java/test/TestClass.java
+++ AutomationPractice1/src/test/java/test/TestClass.java
@@ -4,13 +4,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import main.SingletonDriver;
+import main.UtilClass;
+
 public class TestClass {
 
 	@Test
 	public void testing() {
 		
-		WebDriver driver = new ChromeDriver();
-
-		driver.get("http://automationpractice.com/index.php");
+	//driver.get("http://automationpractice.com/index.php");
+	WebDriver driver;
+	
+	SingletonDriver sd = SingletonDriver.getThisClassInstance();
+	
+	driver = sd.openBrowser("chrome", "https://www.automationpractice.com");
+	
+	UtilClass uc = new UtilClass();
+	
+	uc.closeAllBrowsers();
+	
 	}
 }
